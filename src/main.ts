2b2697cf36638a6588334078a754378d820ea674
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2024-02-27 20:37:00 +0900

    [feat] TransformInterceptor : 성공응답 데이터 래핑

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { Express } from 'express';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './interceptors/transform.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<INestApplication<Express>>(AppModule);
   const port = 5050;
+
+  app.useGlobalInterceptors(new TransformInterceptor());
+
   await app.listen(port, () => {
     console.log(`[BE Server] server is running af port no.${port}`);
   });
