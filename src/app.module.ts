441a9293c7cad01846f9d678b1bf186eb4994844
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2024-02-28 13:32:03 +0900

    [fix] auth guard 적용 및 jwt 인증방식 수정(cookie)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,15 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CoreModule } from './core/core.module';
 import { PrismaModule } from './db/prisma/prisma.module';
 import { DomainsModule } from './domains/domains.module';
+import { AuthGuard } from './guards/auth.guard';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    DomainsModule,
-    PrismaModule,
-  ],
+  imports: [CoreModule, DomainsModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, { provide: APP_GUARD, useClass: AuthGuard }],
 })
 export class AppModule {}
--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,10 +1,60 @@
-import { NextFunction, Request, Response } from 'express';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { AccountType } from 'aws-sdk/clients/chime';
+import { Request } from 'express';
+import { JwtPayload } from 'jsonwebtoken';
+import { PrismaService } from './../db/prisma/prisma.service';
 
-export default function authGuard(
-  req: Request,
-  _: Response,
-  next: NextFunction,
-) {
-  if (!req.user) throw new Error('Unauthorized');
-  next();
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly configService: ConfigService,
+    private readonly prismaService: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const accountTypeInDecorator =
+      this.reflector.getAllAndOverride<AccountType>('accountType', [
+        context.getHandler(),
+        context.getClass(),
+      ]);
+    if (!accountTypeInDecorator) return true;
+
+    const request = context.switchToHttp().getRequest<Request>();
+    const accessToken = request.cookies['accessToken'];
+
+    if (!accessToken) throw new UnauthorizedException('No Access Token');
+
+    try {
+      const secretKey = this.configService.getOrThrow<string>('JWT_SECRET_KEY');
+      const { sub: id, accountType: accountTypeInAccessToken } =
+        this.jwtService.verify(accessToken, {
+          secret: secretKey,
+        }) as JwtPayload & {
+          accountType: AccountType;
+        };
+
+      if (accountTypeInDecorator !== accountTypeInAccessToken)
+        throw new Error();
+
+      if (accountTypeInDecorator === 'user') {
+        const user = await this.prismaService.user.findFirstOrThrow({
+          where: { id },
+        });
+        request.user = user;
+      }
+    } catch (e) {
+      throw new UnauthorizedException(e.message);
+    }
+    return true;
+  }
 }
